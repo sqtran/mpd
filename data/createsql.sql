3c4af3914fcc2aba308f533dd0f44d0e1692a3c0
karabelasw <william@karabelas.com>
2015-08-07 09:37:36 -0400

    Add create sql for traffic violations and csv file for import

--- data/createsql.sql
+++ data/createsql.sql
@@ -1,5 +1,17 @@
 USE mpd;
 
+DROP TABLE IF EXISTS `mpd`.`parkingmovinginfractions`;
+CREATE TABLE  `mpd`.`parkingmovinginfractions` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'AutoIncrement',
+  `Code` varchar(45) CHARACTER SET latin1 NOT NULL COMMENT 'Parking Or Traffic Code',
+  `Description` text CHARACTER SET latin1 NOT NULL,
+  `ShortDescription` varchar(45) CHARACTER SET latin1 NOT NULL COMMENT 'Short Description of Violation',
+  `Fine` decimal(7,2) unsigned NOT NULL COMMENT 'Dollar Fine',
+  `Display` tinyint(1) NOT NULL COMMENT 'Should this row be displayed',
+  PRIMARY KEY (`ID`,`Code`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5801 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Metropolitan Police Departmen';
+
+
 CREATE TABLE  RequestLog (
 	Id int(10) unsigned NOT NULL AUTO_INCREMENT,
 	IpAddress varchar(45) NOT NULL,
